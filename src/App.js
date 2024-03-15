75106921b899775e395a5d304427c6dfdfd247b7
Priyajha7585 <priyajha7585@gmail.com>
2024-03-15 09:08:47 +0530

    Updating app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import Board from "./Components/Board/Board";
+import { User, Sun, Moon } from "react-feather";
+import "./App.css";
+import Editable from "./Components/Editabled/Editable";
 
 function App() {
+  const [theme, setTheme] = useState(sessionStorage.getItem("theme") || true);
+  const [boards, setBoards] = useState(
+    JSON.parse(sessionStorage.getItem("prac-kanban")) || [
+      {
+        id: 0,
+        title: "To Do",
+        cards: [],
+      },
+      {
+        id: 1,
+        title: "In Progress",
+        cards: [],
+      },
+      {
+        id: 2,
+        title: "Done",
+        cards: [],
+      },
+    ]
+  );
+
+  const [targetCard, setTargetCard] = useState({
+    bid: "",
+    cid: "",
+  });
+
+  const addboardHandler = (name) => {
+    const tempBoards = [...boards];
+    tempBoards.push({
+      id: Date.now() + Math.random() * 2,
+      title: name,
+      cards: [],
+    });
+    setBoards(tempBoards);
+  };
+
+  const removeBoard = (id) => {
+    const index = boards.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    tempBoards.splice(index, 1);
+    setBoards(tempBoards);
+  };
+
+  const addCardHandler = (id, title) => {
+    const index = boards.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    tempBoards[index].cards.push({
+      id: Date.now() + Math.random() * 2,
+      title,
+      labels: [],
+      date: "",
+      tasks: [],
+    });
+    setBoards(tempBoards);
+  };
+
+  const removeCard = (bid, cid) => {
+    const index = boards.findIndex((item) => item.id === bid);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    const cards = tempBoards[index].cards;
+
+    const cardIndex = cards.findIndex((item) => item.id === cid);
+    if (cardIndex < 0) return;
+
+    cards.splice(cardIndex, 1);
+    setBoards(tempBoards);
+  };
+
+  const dragEnded = (bid, cid) => {
+    let s_boardIndex, s_cardIndex, t_boardIndex, t_cardIndex;
+    s_boardIndex = boards.findIndex((item) => item.id === bid);
+    if (s_boardIndex < 0) return;
+
+    s_cardIndex = boards[s_boardIndex]?.cards?.findIndex(
+      (item) => item.id === cid
+    );
+    if (s_cardIndex < 0) return;
+
+    t_boardIndex = boards.findIndex((item) => item.id === targetCard.bid);
+    if (t_boardIndex < 0) return;
+
+    t_cardIndex = boards[t_boardIndex]?.cards?.findIndex(
+      (item) => item.id === targetCard.cid
+    );
+    if (t_cardIndex < 0) return;
+
+    const tempBoards = [...boards];
+    const sourceCard = tempBoards[s_boardIndex].cards[s_cardIndex];
+    tempBoards[s_boardIndex].cards.splice(s_cardIndex, 1);
+    tempBoards[t_boardIndex].cards.splice(t_cardIndex, 0, sourceCard);
+    setBoards(tempBoards);
+
+    setTargetCard({
+      bid: "",
+      cid: "",
+    });
+  };
+
+  const dragEntered = (bid, cid) => {
+    if (targetCard.cid === cid) return;
+    setTargetCard({
+      bid,
+      cid,
+    });
+  };
+
+  const updateCard = (bid, cid, card) => {
+    const index = boards.findIndex((item) => item.id === bid);
+    if (index < 0) return;
+
+    const tempBoards = [...boards];
+    const cards = tempBoards[index].cards;
+
+    const cardIndex = cards.findIndex((item) => item.id === cid);
+    if (cardIndex < 0) return;
+
+    tempBoards[index].cards[cardIndex] = card;
+
+    setBoards(tempBoards);
+  };
+
+  const changeTheme = () => {
+    setTheme(!theme);
+  };
+
+  useEffect(() => {
+    sessionStorage.setItem("prac-kanban", JSON.stringify(boards));
+  }, [boards]);
+  useEffect(() => {
+    sessionStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_nav">
+        <h1>DevTrack</h1>
+        <User />
+        <button onClick={changeTheme}>{theme ? <Sun /> : <Moon />}</button>
+      </div>
+      <div className="app_boards_container">
+        <div className="app_boards">
+          {boards.map((item) => (
+            <Board
+              key={item.id}
+              board={item}
+              addCard={addCardHandler}
+              removeBoard={() => removeBoard(item.id)}
+              removeCard={removeCard}
+              dragEnded={dragEnded}
+              dragEntered={dragEntered}
+              updateCard={updateCard}
+            />
+          ))}
+          {/* <div className="app_boards_last">
+            <Editable
+              displayClass="app_boards_add-board"
+              editClass="app_boards_add-board_edit"
+              placeholder="Enter Board Name"
+              text="Add Board"
+              buttonText="Add Board"
+              onSubmit={addboardHandler}
+            />
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
